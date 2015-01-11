98210e288af65974fa9d0225a1617d7965001191
Alvaro Espinel <aespinel@gmail.com>
2015-01-11 16:18:08 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,8 +5,10 @@ d <- readRDS("power.rds")
 
 # Open png device
 png(filename="plot1.png", width=480, height=480)
-png("plot1.png", width=480, height=480)
+
+#Creates the Histogram
 par(mar=c(4.7, 4.7, 0.7, 0.7))
 hist(d$Global_active_power, breaks=12, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
 
+#Turn off device
 dev.off()
